589befa948cdeb65edd7e3dae76da11da2ad3dec
FoxMaSk <foxmaskhome@gmail.com>
2018-09-30 16:59:57 +0200

    improvment of import of JoplinWebApi or JopinCmdApi

--- joplin_api/__init__.py
+++ joplin_api/__init__.py
@@ -3,7 +3,7 @@
     This module allow us to manage our notes / folders / tags into our Joplin Editor
 """
 
-from .core import JoplinApi
+from .core import JoplinApi, JoplinWebApi, JoplinCmdApi
 
 VERSION = (1, 0, 0)  # PEP 386
 __version__ = ".".join([str(x) for x in VERSION])
