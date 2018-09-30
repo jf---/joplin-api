9468a91dddf1b6757fcab99a442c7b992fb278c8
FoxMaSk <foxmaskhome@gmail.com>
2018-09-30 16:40:56 +0200

    add factory to be able to switch from WebApi or CmdApi

--- joplin_api/core.py
+++ joplin_api/core.py
@@ -4,7 +4,7 @@
 
     Usage:
     >>> from joplin_api import JoplinApi
-    >>> joplin = JoplinApi()
+    >>> joplin = JoplinApi('Api')
     >>> joplin.ping()  # to check if the service is up
     >>> joplin.get_folders() # to get all the folders
     >>> folder_title = 'Default'
@@ -19,8 +19,13 @@
 """
 # external lib to use async accesses to the webclipper
 import asks
+import json
 
+import shlex
+from subprocess import Popen, PIPE
 import logging
+from logging import getLogger
+logger = getLogger("joplin_api.api")
 
 __author__ = 'FoxMaSk'
 __all__ = ['JoplinApi']
@@ -29,6 +34,26 @@ logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
 class JoplinApi:
+
+    # factory to switch between API in case we do not use WebClipper
+
+    def factory(api_type, token):
+        """
+
+        :param api_type: type of API to use
+        :param token: string of the unique TOKEN of the API
+        :return:
+        """
+        if api_type == "CmdApi":
+            return JoplinCmdApi(token)
+        if api_type == "Api":
+            return JoplinWebApi(token)
+        assert 0, "Bad Joplin API: " + api_type
+
+    factory = staticmethod(factory)
+
+
+class JoplinWebApi(JoplinApi):
 
     # joplin webclipper service
     JOPLIN_HOST = ''
@@ -211,8 +236,7 @@ class JoplinApi:
         :return: res: json result of the post
         """
         parent_id = kwargs.get('parent_id', 0)
-        data = {'folder': folder,
-                'parent_id': parent_id}
+        data = {'folder': folder, 'parent_id': parent_id}
         return await self.query('post', 'folders', params=data)
 
     async def update_folder(self, folder_id, title, **kwargs):
@@ -225,8 +249,7 @@ class JoplinApi:
         :return: res: json result of the put
         """
         parent_id = kwargs.get('parent_id', 0)
-        data = {'title': title,
-                'parent_id': parent_id}
+        data = {'title': title, 'parent_id': parent_id}
         path = f"/folders/{folder_id}"
         return await self.query('put', path, params=data)
 
@@ -250,8 +273,7 @@ class JoplinApi:
         :param folder: string name of the folder
         :return: res: json result of the put
         """
-        data = {'id': folder_id,
-                'folder': folder}
+        data = {'id': folder_id, 'folder': folder}
         return await self.query('put', 'folders', params=data)
 
     ##############
@@ -323,15 +345,16 @@ class JoplinApi:
         path = f"/tags/{note_id}/notes"
         return await self.query('get', path, params={})
 
-    async def create_tags_notes(self, note_id):
+    async def create_tags_notes(self, note_id, tag):
         """
-        GET /tags/:id/notes
+        POST /tags/:id/notes
 
         create a tag from a note
         :return: res: json result of the get
         """
+        data = {'title': tag}
         path = f"/tags/{note_id}/notes"
-        return await self.query('get', path, params={})
+        return await self.query('post', path, params=data)
 
     async def delete_tags_notes(self, tag_id, note_id):
         """
@@ -343,7 +366,7 @@ class JoplinApi:
         :return: res: json result of the delete
         """
         path = f"/tags/{tag_id}/notes/{note_id}"
-        return await self.query('get', path, params={})
+        return await self.query('delete', path, params={})
 
     ##############
     # RESOURCES
@@ -430,3 +453,244 @@ class JoplinApi:
         if res.text != 'JoplinClipperServer':
             raise ConnectionError('WebClipper unavailable. Check "Tools > Webclipper options" if the service is enable')
         return res
+
+
+class JoplinCmdApi(JoplinApi):
+
+    profile_path = ''
+
+    def __init__(self, token='', **config):
+        """
+        set the profile path of joplin
+        """
+        self.token = token
+        self.profile_path = config.get('JOPLIN_PROFILE_PATH', '')
+
+    def _run(self, line):
+        """
+        Build the command to be running
+        :param line: the command to run
+        :return: message and exitcode
+        """
+        cmd = "joplin --profile {} {}".format(self.profile_path, line)
+        logger.debug(cmd)
+        args = shlex.split(cmd)
+        proc = Popen(args, stdout=PIPE, stderr=PIPE)
+        out, err = proc.communicate()
+        exitcode = proc.returncode
+        logger.info("joplin %s %s %s" % (out, err, exitcode))
+        return out, err, exitcode
+
+    #########
+    # Notes #
+    #########
+    def ls(self, type_object):
+        """
+        list notes, but extract __the line__ of the new created note
+        :param type_object: n = note - t = task
+        :return: message and exitcode
+        """
+        line = " ls -n 1 -s created_time -t {} -f json".format(type_object)
+        return self._run(line)
+
+    def setp(self, note_id, **kwargs):
+        """
+        set properties
+        :param note_id: id on the created note
+        :param kwargs: can contains body and some other additionals properties
+        :return: message and exitcode
+        """
+        out = err = ''
+        exitcode = 0
+        line_start = 'set {note_id} '.format(note_id=note_id)
+        for key in kwargs:
+            line = line_start + ' {key} "{value}"'.format(key=key, value=kwargs.get(key))
+            print("SET {}", line)
+            logger.debug("SET %s " % line)
+            out, err, exitcode = self._run(line)
+
+        return out, err, exitcode
+
+    def create_note(self, notebook, title, body, type_object='n'):
+        """
+        Create a note
+        :param notebook: notebook choosen to store the new note
+        :param title: note title to create
+        :param body: content of the note to add to the created note
+        :param type_object: type of object to create : n = note, t = task
+        :return: message and exitcode
+        """
+        kwargs = dict()
+        out, err, exitcode = self._use(notebook)
+        if exitcode == 0:
+            line = 'mknote "{}"'.format(title)
+            out, err, exitcode = self._run(line)
+            if exitcode == 0:
+                out, err, exitcode = self.ls(type_object)
+                if exitcode == 0:
+                    # 3) set the body of the note
+                    payload = json.loads(out)
+                    note_id = payload[0]['id'][:5]
+                    logger.debug("note id %s " % note_id)
+                    logger.debug("titre %s " % payload[0]['title'])
+                    logger.debug("body  %s " % payload[0]['body'])
+                    logger.debug("todo  %s " % payload[0]['is_todo'])
+                    kwargs['body'] = body
+                    out, err, exitcode = self.setp(note_id, **kwargs)
+        return out, err, exitcode
+
+    def update_note(self, note_id, parent_id, title, body, is_todo):
+        """
+        Edit a note
+        :param note_id: note id to edit
+        :param parent_id: notebook choosen to store the note
+        :param title: note title to update
+        :param body: content of the note edit the note
+        :param is_todo: boolean 1 = todo 0 = note
+        :return: message and exitcode
+        """
+        kwargs = dict()
+        kwargs['parent_id'] = parent_id
+        kwargs['title'] = title
+        kwargs['body'] = body
+        kwargs['is_todo'] = is_todo
+        return self.setp(note_id, **kwargs)
+
+    # def rmnote(self, note):
+    def delete_note(self, note):
+        """
+        Remove a note
+        :param note: id to delete
+        :return: message and exitcode
+        """
+        line = 'rmnote "{}"'.format(note)
+        logger.debug(line)
+        return self._run(line)
+
+    ############
+    # Noteooks #
+    ############
+    # def mkbook(self, notebook):
+    def create_folder(self, notebook):
+        """
+        Create a notebook
+        :param notebook: to create
+        :return: message and exitcode
+        """
+        line = 'mkbook "{}"'.format(notebook)
+        logger.debug(line)
+        return self._run(line)
+
+    # def rmbook(self, notebook):
+    def delete_folder(self, notebook):
+        """
+        Remove a notebook
+        :param notebook: to delete
+        :return: message and exitcode
+        """
+        line = 'rmbook '.format(notebook)
+        logger.debug(line)
+        return self._run(line)
+
+    def _use(self, notebook):
+        """
+        point to the notebook to use
+        :return: message and exitcode
+        """
+        line = 'use "{}"'.format(notebook)
+        logger.debug(line)
+        return self._run(line)
+
+    #########
+    # To-do #
+    #########
+    def toggle(self, note_id):
+        """
+        set a note as a (uncomplet) To-do
+        then set a To-do as complet
+        then set a completed To-do as a (uncompleted) To-do
+        :return: message and exitcode
+        """
+        line = 'todo toggle "{}"'.format(note_id)
+        logger.debug(line)
+        return self._run(line)
+
+    def clear(self, note_id):
+        """
+        set a To-do back as note
+        :return: message and exitcode
+        """
+        line = 'todo clear "{}"'.format(note_id)
+        logger.debug(line)
+        return self._run(line)
+
+    def done(self, note_id):
+        """
+        Marks a to-do as completed.
+        :return: message and exitcode
+        """
+        line = 'done "{}"'.format(note_id)
+        logger.debug(line)
+        return self._run(line)
+
+    def undone(self, note_id):
+        """
+        Marks a to-do as non-completed.
+        :return: message and exitcode
+        """
+        line = 'undone "{}"'.format(note_id)
+        logger.debug(line)
+        return self._run(line)
+
+    ######################
+    # Note and Notebooks #
+    ######################
+    def ren(self, note_or_folder_id, name):
+        """
+        Rename a note or a folder
+        :param note_or_folder_id: id to rename (note or folder)
+        :param name: string to use for renaming
+        :return: message and exitcode
+        """
+        line = 'ren "{}" "{}"'.format(note_or_folder_id, name)
+        logger.debug(line)
+        return self._run(line)
+
+    #######
+    # Tag #
+    #######
+    def tag(self, action, tag_id, note_id):
+        """
+        deal with tag for a note
+        :param action: can be add/remove/list
+        :param tag_id: tag to add or remove to the note
+        :param note_id: id of the note where the tag is added or deleted
+        :return: message and exitcode
+        """
+        line = "tag {action} {tag_id} {note_id}".format(action=action, tag_id=tag_id, note_id=note_id)
+        return self._run(line)
+
+    ###############
+    # Joplin info #
+    ###############
+    def version(self):
+        """
+        Version of Joplin
+        :return: message and exitcode
+        """
+        logger.debug('joplin version ?')
+        return self._run('version')
+
+    ############
+    # Settings #
+    ############
+    def config(self, name, value):
+        """
+        apply a config for `name` to `value`
+        see doc at https://joplin.cozic.net/terminal/  `config [name] [value]`
+        :param name: config name
+        :param value: value to apply
+        :return: message and exitcode
+        """
+        line = "config {name} {value}".format(name=name, value=value)
+        return self._run(line)
